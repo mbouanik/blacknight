8cd4614a3cb46fe0079e32b963a980cdaed1a5f7
miyzu <mbouanik@icloud.com>
2025-04-05 15:09:45 -0500

    ff

--- lua/tokyonight/colors/night.lua
+++ lua/tokyonight/colors/night.lua
@@ -2,7 +2,7 @@ local ret = vim.deepcopy(require("tokyonight.colors.storm"))
 
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
-  bg = "#1a1b26",
-  bg_dark = "#16161e",
-  bg_dark1 = "#0C0E14",
+  bg = "#000000",
+  bg_dark = "#000000",
+  bg_dark1 = "#000000",
 })
